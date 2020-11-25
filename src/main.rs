5f09719da525e730cab798f83f37bf4c8e211b7d
adi <adi@alterra.id>
2020-11-25 22:15:38 +0700

    handle case insensitive

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}.", config.query);
-    println!("In file {}.", config.filename);
-
     if let Err(e) = simple_grep::run(config) {
-        println!("App error: {}", e);
+        eprintln!("App error: {}", e);
         process::exit(1);
     }
 }
